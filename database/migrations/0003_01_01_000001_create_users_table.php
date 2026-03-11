c02bfc4892f75a0d1bcd35b2bd3d2bb86ba1e353
Drew Roberts <github@drewroberts.com>
2026-03-11 12:16:18 -0400

    Remove duplicate relationship wif user

--- database/migrations/0003_01_01_000001_create_users_table.php
+++ database/migrations/0003_01_01_000001_create_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('registration_client_id')->constrained('clients');
             $table->foreignId('agent_id')->nullable()->unique()->index()->constrained('agents');
             $table->text('bio')->nullable();
             $table->json('social_links')->nullable();

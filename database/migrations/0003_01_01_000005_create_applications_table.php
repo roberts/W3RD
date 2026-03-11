b8f0d0999f2978bfd4c67fb7fe171f70ff287a4a
Drew Roberts <github@drewroberts.com>
2026-03-11 18:09:05 -0400

    Add registration parent to applications

--- database/migrations/0003_01_01_000005_create_applications_table.php
+++ database/migrations/0003_01_01_000005_create_applications_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('applications', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('registration_id')->nullable()->constrained('registrations');
             $table->timestamps();
         });
     }

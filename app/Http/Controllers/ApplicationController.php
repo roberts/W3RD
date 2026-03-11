8885e783730dc9d9fd59eed66fc60e49c20c22db
Drew Roberts <github@drewroberts.com>
2026-03-11 12:50:35 -0400

    Stub application model

---
+++ app/Http/Controllers/ApplicationController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreApplicationRequest;
+use App\Http\Requests\UpdateApplicationRequest;
+use App\Models\Application;
+
+class ApplicationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreApplicationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Application $application)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateApplicationRequest $request, Application $application)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Application $application)
+    {
+        //
+    }
+}
